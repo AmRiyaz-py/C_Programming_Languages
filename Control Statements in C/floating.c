cc9687f1b614f82d68a12178d5d959ddb5751dd9
Ammar Riyaz <66881966+AmRiyaz-py@users.noreply.github.com>
2020-07-15 23:18:36 +0530

    Add files via upload

---
+++ Control Statements in C/floating.c
@@ -0,0 +1,16 @@
+/*
+program:-  C program for a floating output
+
+Author:-  Jerry
+
+Last Modified:- July 20
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    int a = 8;
+    float b = 7.333;
+    printf("The value of a %d and the value of b is %f:- ",&a,&b);
+}
