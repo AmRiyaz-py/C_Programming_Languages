27f16f3ee7f7af30ea4a91245a952746e4561780
Ammar Riyaz <66881966+AmRiyaz-py@users.noreply.github.com>
2020-08-08 22:33:05 +0530

    Add files via upload

---
+++ Loop Control Statements in C/ArmStrong.c
@@ -0,0 +1,18 @@
+/*
+Program:- Check the entered number is ArmStrong Number or Not (Ex: 153=1^3 + 5^3 + 3^3)
+
+Author:- AmRiyaz
+
+Last Modifies:- 2020 August
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main(){
+    int n,r,a,j,s=0;
+    printf("Enter the number to check the number of armstrong or not:- ");
+    scanf("%d",&n);
+    a=n;
+    for
+}
