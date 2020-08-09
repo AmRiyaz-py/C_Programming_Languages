399165b3daaefa34f374461890f43ae761f8e224
Ammar Riyaz <66881966+AmRiyaz-py@users.noreply.github.com>
2020-08-10 00:06:13 +0530

    Add files via upload

---
+++ Loop Control Statements in C/digitWhile.c
@@ -0,0 +1,17 @@
+/*
+Program:- Find the sum of individual digit of the entered number (Ex: 153 = 1+5+3=9)
+
+Author:- AmRiyaz
+
+Last Modifies:- 2020 August
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main(){
+    int n,r,s=0;
+    printf("Enter the single number to sum their individual digits:- ");
+    scanf("%d",&n);
+    while
+}
